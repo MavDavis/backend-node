266efb2d051dedd9009659780dcfe2695ad8c694
Mavdavis <ekwesimarvelous@gmail.com>
2023-03-19 17:25:01 +0100

    module export

--- node-js/file2.js
+++ node-js/file2.js
@@ -5,4 +5,14 @@
 
 // const Int = setInterval(() => {
 //     console.log('Intervals');
-// }, 1000);
+// }, 1000); 
+// what I can buikd with node.js :
+/* Traditional website;
+Backend services like APIs;
+Real-time applicaitons;
+CLI tooks
+Muktiplayer games;
+PS: Node.js has a complete javascript runtime environment;
+it allows us to write javasvript outside the browser
+
+*/ 
---
+++ node-js/index.js
@@ -0,0 +1,3 @@
+const file1 = require("./file1")
+console.log(file1.array);
+console.log('index.js running'); 
